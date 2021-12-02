ea2e193ed443358a4a2c15edaf3ca3cfe8ce0b2c
thegister <gian@cercena.com>
2021-12-02 14:05:38 -0500

    Update main.cpp

--- Day 2/main.cpp
+++ Day 2/main.cpp
@@ -18,28 +18,36 @@ int main() {
         //reads in current
         fIn >> action >> curr;
 
+        //this stuff is really straight forward but for the sake of commenting:
+        //if fIn reads action in as "forward"
         if(action.compare("forward") == 0){
-            std::cout << "Horiz: " << horiz << " + " << curr << "\n";
+            //std::cout << "Horiz: " << horiz << " + " << curr << "\n";
+            //add curr to horizontal value
             horiz += curr;
+            //add aim * curr to depth
             depth += aim * curr;
         } else if (action.compare("down") == 0){
-            std::cout << "Depth: " << depth << " + " << curr << "\n";
+            //std::cout << "Depth: " << depth << " + " << curr << "\n";
+            //if action is "down" add curr to aim
             aim += curr;
         } else {
-            std::cout << "Depth: " << depth << " - " << curr << "\n";
+            //std::cout << "Depth: " << depth << " - " << curr << "\n";
+            //if action is "up" subtract curr to aim
             aim -= curr;
         }
+        //clearing fIn
         fIn.clear();
+        //printing stuff
         std::cout << "--------" << "\n";
         std::cout << "Horiz: " << horiz << "\n";
         std::cout << "Depth: " << depth << "\n";
         std::cout << "--------" << "\n";
     }
-
+    //result
     std::cout << horiz*depth;
 
 
-//
+
 //    //Part 1
 //    std::ifstream fIn;
 //
