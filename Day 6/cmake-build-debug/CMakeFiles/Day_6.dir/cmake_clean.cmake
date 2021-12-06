89c52423490eb82fce9c72bf15013f286df92a06
thegister <gian@cercena.com>
2021-12-06 09:59:35 -0500

    Day 6

---
+++ Day 6/cmake-build-debug/CMakeFiles/Day_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day_6.dir/main.cpp.obj"
+  "Day_6.exe"
+  "Day_6.exe.manifest"
+  "Day_6.pdb"
+  "libDay_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Day 6/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main() {
+
+    //this day made me hate longs
+
+    std::ifstream fIn;
+    fIn.open("../aoc_day6.txt");
+
+    char comma;
+    int curr;
+
+    //using long long for everything cause numbers get REAL big
+    std::vector<long long>list(9);
+
+    //reading in
+    while(fIn && fIn.peek() != EOF){
+        fIn >> curr;
+        list[curr] += 1;
+        fIn >> comma;
+    }
+
+    long long temp;
+    int day = 80;
+
+    for(int j = 0; j < 2; j++) {
+        std::vector<long long>listt = list;
+        for (int i = 0; i < day; i++) {
+            //swapping days here
+            //0 go to 6 and 8
+            //others move down by one
+            temp = listt[0];
+            listt[0] = listt[1];
+            listt[1] = listt[2];
+            listt[2] = listt[3];
+            listt[3] = listt[4];
+            listt[4] = listt[5];
+            listt[5] = listt[6];
+            listt[6] = listt[7] + temp;
+            listt[7] = listt[8];
+            listt[8] = temp;
+        }
+
+        long long s = 0;
+        unsigned long long t = 0;
+
+        for (int i = 0; i < 9; i++) {
+            s += listt[i];
+        }
+
+        //printing out and resetting
+        std::cout << "DAY " << day << ": " << s << "\n";
+        s = 0;
+        t = 0;
+        temp = 0;
+
+        day = 256;
+    }
+
+    return 0;
+}
