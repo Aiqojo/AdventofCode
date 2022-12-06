6d1b2219510661684a0f23e803d0df91add1cc85
Aiqojo <gian@cercena.com>
2022-12-06 00:21:54 -0500

    day 6

---
+++ 2022/d6/d6.py
@@ -0,0 +1,23 @@
+p = []
+f = open("t6.txt", "r")
+for line in f:
+    p.append(line.strip())
+
+for line in p:
+    counter = 0
+    listt = []
+    size = 4
+    # size = 14
+    for letter in line:
+        if counter < size:
+            listt.append(letter)
+            counter += 1
+        else:
+            d = set(listt)
+            if len(d) == size:
+                print(counter)
+                break
+            else:
+                listt.pop(0)
+                listt.append(letter)
+                counter += 1
